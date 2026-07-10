6e0a9326496c4fc60ebf3a526eae19cfffa5ac25
Jaromir Wysoglad <jwysogla@redhat.com>
2026-07-10 02:55:55 -0400

    Fix ceilometer plugin unit tests

--- ci/run_tests.sh
+++ ci/run_tests.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 set -ex
 
+# Only the code used in RHOSO is tested, because some of the other plugins need additional external dependencies (like a running Loki). 
 go test -v ./plugins/application/prometheus/... ./plugins/handler/ceilometer-metrics/... ./plugins/transport/socket/... ./pkg/... ./cmd/...
